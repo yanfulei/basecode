9ee8419568ee057dc966a06db9de0245ac1723c3
闫福磊 <yanfulei@foxmail.com>
2018-12-10 20:00:17 +0800

    修改部分样式

---
+++ app/src/main/java/basecode/lsmod/top/ActivityUi.java
@@ -0,0 +1,27 @@
+package basecode.lsmod.top;
+
+import android.view.View;
+
+import top.lsmod.me.basecode.base.BaseActivityTitle;
+
+/**
+ * Author:yanfulei
+ * Date:2018/12/10
+ * Email:yanfulei1990@gmail.com
+ **/
+public class ActivityUi extends BaseActivityTitle {
+    @Override
+    public int setStatusBarColor() {
+        return 0;
+    }
+
+    @Override
+    public View setContentView() {
+        return getLayoutInflater().inflate(R.layout.activity_ui, null);
+    }
+
+    @Override
+    public String setTitleBarText() {
+        return "常用控件";
+    }
+}
