703fbcfed4461083ebe68ebac5b7416065a9ce22
闫福磊 <yanfulei@foxmail.com>
2018-12-18 17:33:49 +0800

    增加自定义属性width

--- baselib/src/main/java/top/lsmod/me/basecode/customui/LeftRightItemView.java
+++ baselib/src/main/java/top/lsmod/me/basecode/customui/LeftRightItemView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,7 +23,10 @@ public class LeftRightItemView extends LinearLayout {
     public String rightText;
     public LinearLayout llAllView;
     public LinearLayout llAllView2;
+    // 间距
     private int lrvPadding;
+    // 左边区域宽度
+    private int lrvLeftWidth;
 
     public LeftRightItemView(Context context) {
         this(context, null);
@@ -41,11 +45,16 @@ public class LeftRightItemView extends LinearLayout {
         leftText = array.getString(R.styleable.LeftRightItemView_left_text);
         rightText = array.getString(R.styleable.LeftRightItemView_right_text);
         lrvPadding = array.getDimensionPixelSize(R.styleable.LeftRightItemView_lrv_padding, 10);
+        lrvLeftWidth = array.getDimensionPixelSize(R.styleable.LeftRightItemView_lrv_left_width, 100);
         array.recycle();
         tvLeftText.setText(leftText);
         tvRightText.setText(rightText);
         // 设置间距
         llAllView2.setPadding(lrvPadding, lrvPadding, lrvPadding, lrvPadding);
+        // 设置左侧宽度
+        ViewGroup.LayoutParams params = tvLeftText.getLayoutParams();
+        params.width = lrvLeftWidth == 0 ? 100 : lrvLeftWidth;
+        tvLeftText.setLayoutParams(params);
     }
 
     public void setOnClickListener(OnClickListener v) {
