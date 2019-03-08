30f769fd4702b407ec2e2e7488e96c08688d8180
闫福磊 <yanfulei@gmail.com>
2019-03-08 16:53:48 +0800

    集成新组建

---
+++ baselib/src/main/java/expandingcollection/BitmapFactoryOptions.java
@@ -0,0 +1,12 @@
+package expandingcollection;
+
+import android.graphics.BitmapFactory;
+
+/**
+ * Bitmap Factory Options with inScaled flag disabled by default
+ */
+public class BitmapFactoryOptions extends BitmapFactory.Options {
+    public BitmapFactoryOptions() {
+        this.inScaled = false;
+    }
+}
