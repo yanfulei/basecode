1385795d92aff4809f624aec76bf83e9fa7e05c3
闫福磊 <yanfulei@gmail.com>
2019-03-19 16:55:54 +0800

    增加高度自适应

---
+++ baselib/src/main/java/top/lsmod/me/basecode/ui/MyGridView.java
@@ -0,0 +1,44 @@
+package top.lsmod.me.basecode.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * @author: yanfulei
+ * @email: yanfulei1990@gmail.com
+ * @description: 高度自适应的GridView
+ * @date: 2019/3/19 4:54 PM
+ */
+public class MyGridView  extends GridView {
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightSpec;
+
+        if (getLayoutParams().height == LayoutParams.WRAP_CONTENT) {
+            // The great Android "hackatlon", the love, the magic.
+            // The two leftmost bits in the height measure spec have
+            // a special meaning, hence we can't use them to describe height.
+            heightSpec = MeasureSpec.makeMeasureSpec(
+                    Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        }
+        else {
+            // Any other height should be respected as is.
+            heightSpec = heightMeasureSpec;
+        }
+
+        super.onMeasure(widthMeasureSpec, heightSpec);
+    }
+}
