593a63364f6a26ef76c703e23cc1ac3bbc170ef8
闫福磊 <yanfulei@gmail.com>
2019-05-30 11:28:59 +0800

    增加fragment基类

---
+++ baselib/src/main/java/top/lsmod/me/basecode/base/BaseFragment.java
@@ -0,0 +1,19 @@
+package top.lsmod.me.basecode.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * @author: yanfulei
+ * @email: yanfulei1990@gmail.com
+ * @description: Fragment基类
+ * @date: 2019-05-30 11:27
+ */
+public class BaseFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
